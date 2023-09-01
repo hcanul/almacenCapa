5459233736b716b1c5c3568d4aab7288135dea0e
Hugo Canul <cyber.frenetic@gmail.com>
2023-09-01 12:14:36 -0500

    ultima actualización

---
+++ app/Http/Livewire/Storehouse/WarehouseRequest/DeparturesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Storehouse\WarehouseRequest;
+
+use Livewire\Component;
+
+class DeparturesController extends Component
+{
+    public function render()
+    {
+        return view('livewire.storehouse.warehouse-request.component');
+    }
+}
